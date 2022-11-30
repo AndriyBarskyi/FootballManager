6fb1693280a31743941d437e695478f57765cfe7
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2022-11-30 19:08:22 +0200

    create initial project

---
+++ src/test/java/com/task/test/footballmanager/FootballManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.task.test.footballmanager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(2, 1+1);
+    }
+
+}
